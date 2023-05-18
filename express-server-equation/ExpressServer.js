b0d60184e55c8b1226c96aa04f85fe4c23768c91
vickneee <victoria.vavulina@outlook.com>
2023-05-18 15:39:26 +0300

    Initial commit

--- express-server-equation/ExpressServer.js
+++ express-server-equation/ExpressServer.js
@@ -6,44 +6,44 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function(req, res, next) {
-	var d = new Date();
+	const d = new Date();
 	console.log(d + ' Request Type:', req.method + ' ' + req.path)
 	next();
 });
+
 app.use(express.static(__dirname));
-// app.get("/", (req, res) => {
-//	// res.send('<html><h1>Hello, I am at your service!</h1></html>');
-//	res.sendFile(__dirname + "/Hello.html");
-//});
 
 app.get("/", (req, res) => {
-	// res.send('<html><h1>Hello, I am at your service!</h1></html>');
 	res.sendFile(__dirname + "/Equation.html");
 });
 
 app.post("/Equation", (req, res) => {
-	var data = [];
+	let message;
+	const data = [];
 	const a = parseInt(req.body.a)
 	const b = parseInt(req.body.b)
 	const c = parseInt(req.body.c)
 	const delta = Math.pow(b, 2) - 4*a*c
 	const equation = a + '*X**2' + b + '*X' + c + '= 0'
+	let x1;
+	let x2;
 	if (delta < 0) {
-		var message = 'Delta < 0, No solution'
+		message = 'Delta < 0, No solution';
 		x1 = ""
-		x2 = ""	
+		x2 = ""
 	}
-	if (delta == 0) {
-		var message = 'Delta = 0, 1 solutions'
+	if (delta === 0) {
+		message = 'Delta = 0, 1 solutions';
 		x1 = (-b) / (2*a)
 		x2 = (-b) / (2*a)
 		console.log('x1 = x2 = ' + x1)
 	}
+	let sqrtdelta;
 	if (delta > 0) {
 		sqrtdelta = Math.sqrt(delta)
-		x1 = (-b + sqrtdelta) / (2*a)
-		x2 = (-b - sqrtdelta) / (2*a)
-		var message = 'Delta > 0, 2 solutions'
+		x1 = (-b + sqrtdelta) / (2 * a)
+		x2 = (-b - sqrtdelta) / (2 * a)
+		message = 'Delta > 0, 2 solutions';
 		console.log('x1 = ' + x1 + 'x2 = ' + x2)
 	}
 	console.log(equation)
@@ -70,8 +70,8 @@ app.get('/Google', (req, res) => {
 	res.send('<a href="https://www.google.com"> Google </a>')
 });
 h = '127.0.0.1'
-var server = app.listen(3200, h, function() {
-	var host = server.address().address;
-	var port = server.address().port;
-	console.log('Server is listening at http://%s:%s', host, port);
+const server = app.listen(3200, h, function () {
+	const host = server.address().address;
+	const port = server.address().port;
+	console.log('Server is listening at https://%s:%s', host, port);
 });
